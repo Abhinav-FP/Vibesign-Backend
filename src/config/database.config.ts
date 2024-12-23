0e2262af5d3036e8334f52d68a994e72dfe6c387
stemy <stemelr.balazs.akos@gmail.com>
2024-12-23 09:32:30 +0100

    Small fixes.

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { MongooseModuleFactoryOptions } from '@nestjs/mongoose';
 
 export default registerAs('database', () => ({
-    uri: process.env.DATABASE_URI || 'dsfdsf',
+    uri: process.env.DATABASE_URI || 'mongodb://localhost:27017',
     dbName: process.env.DATABASE_NAME || 'vibesign',
     user: process.env.DATABASE_USER || '',
     pass: process.env.DATABASE_PASSWORD || '',
