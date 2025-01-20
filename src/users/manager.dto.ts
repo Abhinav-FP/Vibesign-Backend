b4809916511e16065760dfe9182f34025a0e591a
stemy <stemelr.balazs.akos@gmail.com>
2025-01-20 11:59:30 +0100

    Remove host from unnecessary list dto-s

--- src/users/manager.dto.ts
+++ src/users/manager.dto.ts
@@ -1,7 +1,7 @@
 import {OmitType} from '@nestjs/swagger';
 import {AddUserDto, EditUserDto, ListUserDto} from './user.dto';
 
-export class ListManagerDto extends OmitType(ListUserDto, ['devices']) {
+export class ListManagerDto extends OmitType(ListUserDto, ['host', 'devices']) {
 }
 
 export class AddManagerDto extends OmitType(AddUserDto, ['deviceLimit']) {
--- src/users/partner.dto.ts
+++ src/users/partner.dto.ts
@@ -1,7 +1,7 @@
 import {OmitType} from '@nestjs/swagger';
 import {AddUserDto, EditUserDto, ListUserDto} from './user.dto';
 
-export class ListPartnerDto extends OmitType(ListUserDto, ['phone', 'devices']) {
+export class ListPartnerDto extends OmitType(ListUserDto, ['host', 'phone', 'devices']) {
 }
 
 export class AddPartnerDto extends OmitType(AddUserDto, ['phone', 'deviceLimit']) {
